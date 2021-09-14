ae1e41a02e493ed70a3302d7e6348e3d838ce038
Ondřej Samsounek <samsounek.2018@skola.ssps.cz>
2021-09-14 14:16:56 +0200

    all classes done without constructors

--- Sphere.cs
+++ Sphere.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace AbstractClassPractice
 {
-    class Sphere
+    class Sphere : Body
     {
+        double radius;
+        public double Radius
+        {
+            get { return radius; }
+            set
+            {
+                if (value > 0) { radius = value; }
+                else { throw new ArgumentException("Radius of the sphere must be greather than zero!"); }
+            }
+        }
+
+        public override double Surface() { return 4 * Math.PI * Math.Pow(Radius, 2); }
+        public override double Volume() { return 4 * Math.PI * Math.Pow(Radius, 3) / 3; }
     }
 }
