358a9f4bca3281959b1b4e33e0d9280c2ea6a296
Ondřej Samsounek <samsounek.2018@skola.ssps.cz>
2021-09-14 14:11:26 +0200

    class Cylinder done

--- Cylinder.cs
+++ Cylinder.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace AbstractClassPractice
 {
-    class Cylinder
+    class Cylinder : Body //česky válec
     {
+        double height;
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0) { height = value; }
+                else { throw new ArgumentException("Height of the cylinder must be greather than zero!"); }
+            }
+        }
+
+        double baseRadius;
+        public double BaseRadius
+        {
+            get { return baseRadius; }
+            set
+            {
+                if (value > 0) { baseRadius = value; }
+                else { throw new ArgumentException("Radius of the base of the cylinder must be greather than zero!"); }
+            }
+        }
+
+        public override double Surface() { return  2 * Math.PI * BaseRadius * (BaseRadius + Height); }
+        public override double Volume() { return Math.PI * Math.Pow(BaseRadius, 2) * Height; }
     }
 }
