de70df17eea4c030033ac6d08a6b964fcc0e3f9f
Ondřej Samsounek <samsounek.2018@skola.ssps.cz>
2021-09-14 14:00:07 +0200

    class Cube complete

--- Cube.cs
+++ Cube.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace AbstractClassPractice
 {
-    class Cube
+    class Cube : Body
     {
+        double edge;
+        public double Edge
+        {
+            get { return edge; }
+            set
+            {
+                if (value > 0) { edge = value; }
+                else { throw new ArgumentException("Edge of the cube must be greather than zero!"); }
+            }
+        }
+
+        public override double Surface() { return 6 * Math.Pow(Edge, 2); }
+        public override double Volume() { return Math.Pow(Edge, 3); }
     }
 }
