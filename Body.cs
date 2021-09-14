f7facc86780370c767e3447da7f60ae8c806928a
Ondřej Samsounek <samsounek.2018@skola.ssps.cz>
2021-09-14 13:20:04 +0200

    class Body completed

---
+++ Body.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassPractice
+{
+    public abstract class Body
+    {
+        public abstract double Surface();
+        public abstract double Volume();
+        public double SurfaceToVolumeRatio()
+        {
+            return Surface() / Volume();
+        }
+    }
+}
